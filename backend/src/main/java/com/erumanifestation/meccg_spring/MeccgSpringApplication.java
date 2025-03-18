d362bc3bbe48979feeaca626937bfadd26c96992
eru-manifestation <129109001+eru-manifestation@users.noreply.github.com>
2025-03-18 22:24:20 +0100

    feature: initialize backend and frontend

---
+++ backend/src/main/java/com/erumanifestation/meccg_spring/MeccgSpringApplication.java
@@ -0,0 +1,13 @@
+package com.erumanifestation.meccg_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeccgSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeccgSpringApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/erumanifestation/meccg_spring/MeccgSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erumanifestation.meccg_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeccgSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
