2ad160b53659c8cb7fb52a3e51ea8e52c5ac7ee5
eru-manifestation <129109001+eru-manifestation@users.noreply.github.com>
2025-03-20 22:25:46 +0100

    refactor: Remove dotenv dependency and hardcode database credentials in launch configuration

--- backend/src/main/java/com/erumanifestation/meccg_spring/MeccgSpringApplication.java
+++ backend/src/main/java/com/erumanifestation/meccg_spring/MeccgSpringApplication.java
@@ -3,18 +3,11 @@ package com.erumanifestation.meccg_spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class MeccgSpringApplication {
 
 	public static void main(String[] args) {
-		// Load environment variables from .env
-        Dotenv dotenv = Dotenv.load();
-        System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
-        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-
 		SpringApplication.run(MeccgSpringApplication.class, args);
 	}
 
